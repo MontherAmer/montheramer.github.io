1ccae13e870525bd17b49ae6ba42d1efee7b4707
montheramer <monther.amer48@gmail.com>
2021-01-02 00:29:14 +0200

    CREATE function to convert from timestamp to human time

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,12 @@ export const convertMeterperSecToKmPerHour = number => {
   let result = Math.round(num * 10) / 10;
   return result;
 };
+
+/* ------------------convert timestamp to readable time---------------------- */
+export const convertTimeStampToReadableTime = data => {
+  let dateObj = new Date(data * 1000);
+  let hours = dateObj.getHours();
+  let minutes = dateObj.getMinutes();
+  let time = `${hours < 10}?0${hours}:${hours}:${minutes < 10}?0${minutes}:${minutes} ${hours >= 12 ? 'PM' : 'AM'}`;
+  return time;
+};
