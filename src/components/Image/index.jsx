822683a3a8cc892a87de8147ec037deb81bd845f
montheramer <monther.amer48@gmail.com>
2021-01-02 14:39:42 +0200

    CREATE image componnent

---
+++ src/components/Image/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import icon2 from '../../assets/icons/icon2.svg';
+import icon3 from '../../assets/icons/icon3.svg';
+import icon5 from '../../assets/icons/icon5.svg';
+import icon6 from '../../assets/icons/icon6.svg';
+import icon7 from '../../assets/icons/icon7.svg';
+import icon8 from '../../assets/icons/icon8.svg';
+import clear from '../../assets/icons/clear.svg';
+import sunrise from '../../assets/sunrise.svg';
+import sunset from '../../assets/sunset.svg';
+import wind from '../../assets/wind.png';
+
+export default ({ value, width }) => {
+  const images = { icon2, icon3, icon5, icon6, icon7, icon8, clear, sunrise, sunset, wind };
+
+  const ImageName = images[value];
+
+  return <img src={ImageName} alt={value} style={{ width: `${width}px` }} className='image-container' />;
+};
