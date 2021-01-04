572dbb32c66774482b19e08a1be6362ec9226fbd
montheramer <monther.amer48@gmail.com>
2021-01-04 23:28:35 +0200

    ADD functionality to multilocations component

--- src/components/MultiLocations/index.jsx
+++ src/components/MultiLocations/index.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { getMultiForecast } from '../../store/actions';
+
+import Button from './Button';
+import Table from './Table';
+import Map from '../Map';
 
 import './index.css';
 export default ({ hideModal }) => {
+  const dispatch = useDispatch();
+  const [state, setState] = useState({});
+
+  const toggleMap = () => setState({ ...state, showMap: !state.showMap });
+
+  const handleMapClicked = async latLng => {
+    console.log(latLng);
+    await dispatch(getMultiForecast({ latitude: latLng[0], longitude: latLng[1] }));
+    toggleMap();
+  };
+
   return (
     <div className='multiple-locations'>
-      <div></div>
-      <div className='add-location'>
-        <div className='add-button'>
-          <span>Add</span>
-        </div>
-        <div className='sun'></div>
-      </div>
+      {state.showMap ? <Map handlePosition={handleMapClicked} /> : <Table />}
+      <Button onClick={toggleMap} />
     </div>
   );
 };
