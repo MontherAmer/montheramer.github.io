ad513f1f70da93d422e5496cb7c8c2bda5cad450
montheramer <monther.amer48@gmail.com>
2021-01-04 23:25:18 +0200

    ADD images to Image component

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import loc from '../../assets/loc.svg';
+import wind from '../../assets/wind.png';
+import close from '../../assets/close.svg';
+import sunset from '../../assets/sunset.svg';
+import compare from '../../assets/compare.svg';
+import sunrise from '../../assets/sunrise.svg';
 import icon2 from '../../assets/icons/icon2.svg';
 import icon3 from '../../assets/icons/icon3.svg';
 import icon5 from '../../assets/icons/icon5.svg';
@@ -7,13 +13,9 @@ import icon6 from '../../assets/icons/icon6.svg';
 import icon7 from '../../assets/icons/icon7.svg';
 import icon8 from '../../assets/icons/icon8.svg';
 import clear from '../../assets/icons/clear.svg';
-import sunrise from '../../assets/sunrise.svg';
-import sunset from '../../assets/sunset.svg';
-import wind from '../../assets/wind.png';
-import close from '../../assets/close.svg';
 
 export default ({ value, width, classess, onClick }) => {
-  const images = { icon2, icon3, icon5, icon6, icon7, icon8, clear, sunrise, sunset, wind, close };
+  const images = { icon2, icon3, icon5, icon6, icon7, icon8, clear, sunrise, sunset, wind, close, compare, loc };
 
   const ImageName = images[value];
 
