f5594ddc2a61365b04cf7cfb8a94ad98882465d8
montheramer <monther.amer48@gmail.com>
2021-01-04 18:27:18 +0200

    CREATE component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,25 +1,17 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-
-import { getDailyForecast, toggleLoader } from '../../store/actions';
 
 import menuIcon from '../../assets/menu.svg';
 
 import Modal from '../Modal';
-import Map from '../Map';
+import OneLocation from '../OneLocation';
 
 import './index.css';
 export default () => {
-  const dispatch = useDispatch();
   const [state, setState] = useState({});
 
   const toggleMenu = e => setState({ ...state, showMenu: !state.showMenu });
 
   const toggleModal = e => setState({ ...state, showModal: !state.showModal });
-
-  const handlePosition = async latLng => (
-    dispatch(toggleLoader()), await dispatch(getDailyForecast({ latitude: latLng[0], longitude: latLng[1] })), dispatch(toggleLoader())
-  );
 
   return (
     <nav>
@@ -38,7 +30,7 @@ export default () => {
       </div>
       {state.showModal ? (
         <Modal show={state.showModal} hide={toggleModal}>
-          <Map handlePosition={handlePosition} />
+          <OneLocation hideModal={toggleModal} />
         </Modal>
       ) : null}
     </nav>
