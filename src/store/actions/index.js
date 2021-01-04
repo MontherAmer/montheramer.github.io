936301857c8d727341d7bbd044d4fb91d4deb733
montheramer <monther.amer48@gmail.com>
2021-01-04 23:24:13 +0200

    ADD function to get data for multiple weather

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -5,6 +5,29 @@ import { getDayMonth, getDayName, convertMeterperSecToKmPerHour, convertTimeStam
 // * default value of current location if the user does not allow locaion is Greenwich
 const defaultLocation = { type: actionTypes.UPDATE_CURENT_LOCATION, payload: { latitude: 51.477928, longitude: -0.001545 } };
 
+const formationData = data => {
+  return data.list.map((item, i) =>
+    i === 0
+      ? {
+          day: getDayName(i),
+          date: getDayMonth(i),
+          city: data.city.name,
+          dayTemp: Math.round(item.temp.day),
+          nightTemp: Math.round(item.temp.night),
+          speed: convertMeterperSecToKmPerHour(item.speed),
+          sunRise: convertTimeStampToReadableTime(item.sunrise),
+          sunSet: convertTimeStampToReadableTime(item.sunset),
+          icon: getIcon(item.weather[0].id)
+        }
+      : {
+          day: getDayName(i),
+          dayTemp: Math.round(item.temp.day),
+          nightTemp: Math.round(item.temp.night),
+          icon: getIcon(item.weather[0].id)
+        }
+  );
+};
+
 /* ----------------------get user curent location---------------------------- */
 export const getCurentLocation = () => async dispatch => {
   try {
@@ -40,32 +63,35 @@ export const getDailyForecast = ({ latitude, longitude }) => async dispatch => {
     });
 
     if (data) {
-      let list = data.list.map((item, i) =>
-        i === 0
-          ? {
-              day: getDayName(i),
-              date: getDayMonth(i),
-              city: data.city.name,
-              dayTemp: Math.round(item.temp.day),
-              nightTemp: Math.round(item.temp.night),
-              speed: convertMeterperSecToKmPerHour(item.speed),
-              sunRise: convertTimeStampToReadableTime(item.sunrise),
-              sunSet: convertTimeStampToReadableTime(item.sunset),
-              icon: getIcon(item.weather[0].id)
-            }
-          : {
-              day: getDayName(i),
-              dayTemp: Math.round(item.temp.day),
-              nightTemp: Math.round(item.temp.night),
-              icon: getIcon(item.weather[0].id)
-            }
-      );
+      let list = formationData(data);
       dispatch({ type: actionTypes.UPDATE_MAIN_WEATHER_DATA, payload: list });
       dispatch({ type: actionTypes.TOGGLE_LOADER });
     }
   } catch (err) {
     dispatch({ type: actionTypes.TOGGLE_LOADER });
   }
+};
+
+export const getMultiForecast = ({ latitude, longitude }) => async dispatch => {
+  try {
+    dispatch({ type: actionTypes.TOGGLE_LOADER });
+    let { data } = await Axios.get(`${process.env.REACT_APP_WEATHER_MAIN_URL}/daily?lat=${latitude}&lon=${longitude}&cnt=7&units=metric`, {
+      headers: {
+        'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
+      }
+    });
+    if (data) {
+      let list = formationData(data);
+      dispatch({ type: actionTypes.ADD_LOCATION_TO_MULTI_ARRAY, payload: list });
+      dispatch({ type: actionTypes.TOGGLE_LOADER });
+    }
+  } catch (err) {
+    dispatch({ type: actionTypes.TOGGLE_LOADER });
+  }
+};
+
+export const removeItemMultiForecast = index => dispatch => {
+  dispatch({ type: actionTypes.REMOVE_LOCATION_FROM_MULTI_ARRAY, payload: index });
 };
 
 export const toggleLoader = () => dispatch => dispatch({ type: actionTypes.TOGGLE_LOADER });
--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -8,13 +8,20 @@ const initialState = {
 };
 
 export default (state = initialState, { type, payload }) => {
+  let temp;
   switch (type) {
     case actionTypes.UPDATE_CURENT_LOCATION:
       return { ...state, curentLocation: payload };
     case actionTypes.UPDATE_MAIN_WEATHER_DATA:
-      let temp = state.multiLocations;
+      temp = state.multiLocations;
       temp[0] = payload;
       return { ...state, multiLocations: temp, mainWeather: payload };
+    case actionTypes.ADD_LOCATION_TO_MULTI_ARRAY:
+      return { ...state, multiLocations: state.multiLocations.concat([payload]) };
+    case actionTypes.REMOVE_LOCATION_FROM_MULTI_ARRAY:
+      temp = state.multiLocations;
+      temp.splice(payload, 1);
+      return { ...state, multiLocations: temp };
     case actionTypes.TOGGLE_LOADER:
       return { ...state, showLoader: !state.showLoader };
     default:
