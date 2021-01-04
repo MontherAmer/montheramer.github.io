5ad97df4dece51299edd54481bef3d2c0d9d9bc7
montheramer <monther.amer48@gmail.com>
2021-01-04 20:15:42 +0200

    ADD weather to multiweather array when choose new wither

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -3,6 +3,7 @@ import actionTypes from '../actions_types';
 const initialState = {
   curentLocation: {},
   mainWeather: [],
+  multiLocations: [],
   showLoader: false
 };
 
@@ -11,7 +12,9 @@ export default (state = initialState, { type, payload }) => {
     case actionTypes.UPDATE_CURENT_LOCATION:
       return { ...state, curentLocation: payload };
     case actionTypes.UPDATE_MAIN_WEATHER_DATA:
-      return { ...state, mainWeather: payload };
+      let temp = state.multiLocations;
+      temp[0] = payload;
+      return { ...state, multiLocations: temp, mainWeather: payload };
     case actionTypes.TOGGLE_LOADER:
       return { ...state, showLoader: !state.showLoader };
     default:
