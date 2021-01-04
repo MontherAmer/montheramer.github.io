bbf2879bb2264ea748f10bbdeffcdb9cb81d2d2f
montheramer <monther.amer48@gmail.com>
2021-01-04 20:11:04 +0200

    FIX loading when open the app

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -31,33 +31,40 @@ export const getCurentLocation = () => async dispatch => {
 
 /* ----------------------------get foercast weather-------------------------- */
 export const getDailyForecast = ({ latitude, longitude }) => async dispatch => {
-  let { data } = await Axios.get(`${process.env.REACT_APP_WEATHER_MAIN_URL}/daily?lat=${latitude}&lon=${longitude}&cnt=7&units=metric`, {
-    headers: {
-      'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
+  try {
+    dispatch({ type: actionTypes.TOGGLE_LOADER });
+    let { data } = await Axios.get(`${process.env.REACT_APP_WEATHER_MAIN_URL}/daily?lat=${latitude}&lon=${longitude}&cnt=7&units=metric`, {
+      headers: {
+        'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
+      }
+    });
+
+    if (data) {
+      let list = data.list.map((item, i) =>
+        i === 0
+          ? {
+              day: getDayName(i),
+              date: getDayMonth(i),
+              city: data.city.name,
+              dayTemp: Math.round(item.temp.day),
+              nightTemp: Math.round(item.temp.night),
+              speed: convertMeterperSecToKmPerHour(item.speed),
+              sunRise: convertTimeStampToReadableTime(item.sunrise),
+              sunSet: convertTimeStampToReadableTime(item.sunset),
+              icon: getIcon(item.weather[0].id)
+            }
+          : {
+              day: getDayName(i),
+              dayTemp: Math.round(item.temp.day),
+              nightTemp: Math.round(item.temp.night),
+              icon: getIcon(item.weather[0].id)
+            }
+      );
+      dispatch({ type: actionTypes.UPDATE_MAIN_WEATHER_DATA, payload: list });
+      dispatch({ type: actionTypes.TOGGLE_LOADER });
     }
-  });
-  if (data) {
-    let list = data.list.map((item, i) =>
-      i === 0
-        ? {
-            day: getDayName(i),
-            date: getDayMonth(i),
-            city: data.city.name,
-            dayTemp: Math.round(item.temp.day),
-            nightTemp: Math.round(item.temp.night),
-            speed: convertMeterperSecToKmPerHour(item.speed),
-            sunRise: convertTimeStampToReadableTime(item.sunrise),
-            sunSet: convertTimeStampToReadableTime(item.sunset),
-            icon: getIcon(item.weather[0].id)
-          }
-        : {
-            day: getDayName(i),
-            dayTemp: Math.round(item.temp.day),
-            nightTemp: Math.round(item.temp.night),
-            icon: getIcon(item.weather[0].id)
-          }
-    );
-    dispatch({ type: actionTypes.UPDATE_MAIN_WEATHER_DATA, payload: list });
+  } catch (err) {
+    dispatch({ type: actionTypes.TOGGLE_LOADER });
   }
 };
 
