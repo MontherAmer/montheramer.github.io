50df25c9b10fc0a91fd15402653ca8c28ef2bd42
montheramer <monther.amer48@gmail.com>
2021-01-14 21:39:34 +0200

    CREATE actions and reducers to handle updating unit

--- src/store/reducer/_main.js
+++ src/store/reducer/_main.js
@@ -1,17 +1,23 @@
-import { mainActionsTypes } from '../actions_types';
-
+import { mainActionsTypes, utilsActionsTypes } from '../actions_types';
+import { updateUnitHelper } from '../../utils';
 const initialState = {
-  weathers: []
+  weathers: [],
+  coords: []
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case mainActionsTypes.ADD_NEW_WEATHER_ITEM:
-      return { ...state, weathers: state.weathers.concat(payload) };
+      return { ...state, weathers: state.weathers.concat(payload), coords: state.coords.concat(payload[0][0].coord) };
     case mainActionsTypes.REMOVE_WEATHER_ITEM:
       let newWeathers = state.weathers;
       newWeathers.splice(payload, 1);
-      return { ...state, weathers: newWeathers };
+      let newCoords = state.coords;
+      newCoords.splice(payload, 1);
+      return { ...state, weathers: newWeathers, coords: newCoords };
+    case utilsActionsTypes.UPDATE_UNIT:
+      let newArray = updateUnitHelper(state.weathers, payload);
+      return { ...state, weathers: newArray };
     default:
       return state;
   }
--- src/store/reducer/_utils.js
+++ src/store/reducer/_utils.js
@@ -3,7 +3,8 @@ import { utilsActionsTypes } from '../actions_types';
 const initialState = {
   unit: 'metric',
   isMapShown: false,
-  isLoading: false
+  isLoading: false,
+  onlyOneLocation: true
 };
 
 export default (state = initialState, { type, payload }) => {
@@ -16,6 +17,11 @@ export default (state = initialState, { type, payload }) => {
       return { ...state, isLoading: true };
     case utilsActionsTypes.HIDE_LOADER:
       return { ...state, isLoading: false };
+    case utilsActionsTypes.UPDATE_UNIT:
+      return { ...state, unit: payload };
+    case utilsActionsTypes.UPDATE_ONLY_ONE_LOCATION:
+      console.log('heisadkfj ');
+      return { ...state, onlyOneLocation: !state.onlyOneLocation };
     default:
       return state;
   }
